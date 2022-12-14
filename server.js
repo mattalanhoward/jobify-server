9d18b15588c510d169e38b019eacc08ecce374e9
Matthew Howard <mattalanhoward@gmail.com>
2022-12-13 22:03:41 -0500

    Setup Auth / Job Controllers and Routes

--- server.js
+++ server.js
@@ -4,18 +4,27 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 
+// db and authenticateUser
 import connectDB from "./db/connect.js";
+
+// routers
+import authRoutes from "./routes/authRoutes.js";
+import jobsRoutes from "./routes/jobsRoutes.js";
 
 // middleware
 import notFoundMiddleware from "./middleware/not-found.js";
-notFoundMiddleware;
-
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+notFoundMiddleware;
 errorHandlerMiddleware;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("welcome");
 });
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/jobs", jobsRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
